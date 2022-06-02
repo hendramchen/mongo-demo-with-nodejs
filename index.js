f33494bee86ec76ff47053bf0f6b5468c3c86a84
Hendra McHen <mchen.putu@gmail.com>
2022-06-02 21:56:00 +0800

    add remove document

--- index.js
+++ index.js
@@ -38,38 +38,37 @@ async function getCourses() {
   // get all courses without filtering
   // const courses = await Course.find();
   // get all courses with filtering
-  const courses = await Course
-  .find({author: 'Mosh', isPublished: true})
-  .limit(10)
-  .sort({name: 1}) // 1 is ascending, -1 is descending
-  .select({name: 1, tags: 1}); // select only the name and tags fields
+  const courses = await Course.find({ author: "Mosh", isPublished: true })
+    .limit(10)
+    .sort({ name: 1 }) // 1 is ascending, -1 is descending
+    .select({ name: 1, tags: 1 }); // select only the name and tags fields
   console.log(courses);
 }
 
 // async function getCoursesWithComplexFilter() {
-  // eq is equal to
-  // ne is not equal to
-  // gt is greater than
-  // gte is greater than or equal to
-  // lt is less than
-  // lte is less than or equal to
-  // in is in
-  // nin is not in
+// eq is equal to
+// ne is not equal to
+// gt is greater than
+// gte is greater than or equal to
+// lt is less than
+// lte is less than or equal to
+// in is in
+// nin is not in
 
-  // const courses = await Course
-  // .find({ price: { $gte: 10, $lte: 20 } })
+// const courses = await Course
+// .find({ price: { $gte: 10, $lte: 20 } })
 
-  // const courses = await Course
-  // .find({ price: { $in: [10, 15, 20] } })
-  // console.log(courses);
+// const courses = await Course
+// .find({ price: { $in: [10, 15, 20] } })
+// console.log(courses);
 
-  // const courses = await Course
-  // Start with Putu
-  // .find({ author: /^Putu/ })
-  // Ends with Parman
-  // .find({ author: /Parman$/i })
-  // Contains Eka
-  // .find({ author: /.*Eka.*/i })
+// const courses = await Course
+// Start with Putu
+// .find({ author: /^Putu/ })
+// Ends with Parman
+// .find({ author: /Parman$/i })
+// Contains Eka
+// .find({ author: /.*Eka.*/i })
 // }
 
 // about pagination
@@ -90,25 +89,48 @@ async function updateCourseFirstApproach(id) {
   // findById()
   // Modify its properties
   // save()
-  const course =  await Course.findById(id);
+  const course = await Course.findById(id);
   if (!course) return;
 
   course.isPublished = true;
-  course.author = 'Another Author';
+  course.author = "Another Author";
 
   const result = await course.save();
   console.log(result);
 }
 
 async function updateCourseSecondApproach(id) {
-  const result = await Course.updateOne({ _id: id}, {
-    $set: {
-      author: 'Mosh',
-      isPublished: false
+  const result = await Course.updateOne(
+    { _id: id },
+    {
+      $set: {
+        author: "Mosh",
+        isPublished: false,
+      },
     }
-  });
+  );
+  console.log(result);
+}
+
+async function updateCourseThirdApproach(id) {
+  const course = await Course.findByIdAndUpdate(
+    id,
+    {
+      $set: {
+        author: "Mosh",
+        isPublished: false,
+      },
+    },
+    { new: true }
+  );
+  console.log(course);
+}
+
+async function removeCourse(id) {
+  const result = await Course.deleteOne({ _id: id });
   console.log(result);
 }
 
 // updateCourseFirstApproach('629762d47de482ebb4c67209');
-updateCourseSecondApproach('62976316c2bda3446f2c15b3');
+// updateCourseSecondApproach("62976316c2bda3446f2c15b3");
+removeCourse('62976316c2bda3446f2c15b3');
